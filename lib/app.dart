a4a636352aa495650052ca07fb3420d70a579906
amaan-0331 <127935561+amaan-honorfx@users.noreply.github.com>
2024-02-13 11:49:23 +0400

    initial commit

---
+++ lib/app.dart
@@ -0,0 +1,13 @@
+import 'package:bloc_example/src/splash_view.dart';
+import 'package:flutter/material.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: SplashView(),
+    );
+  }
+}
