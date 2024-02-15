9de29cab584efc2b2b1440987b55cd7e94244b53
amaan-0331 <127935561+amaan-honorfx@users.noreply.github.com>
2024-02-15 15:43:34 +0400

    feat(user_repository): add base models

--- packages/user_repository/lib/src/entities/current_user_entity.dart
+++ packages/user_repository/lib/src/entities/current_user_entity.dart
@@ -1,1 +1,46 @@
-class CurrentUserEntity {}
+import 'package:equatable/equatable.dart';
+
+class CurrentUserEntity extends Equatable {
+  final String id;
+  final String email;
+  final String name;
+  final String? picture;
+
+  const CurrentUserEntity({
+    required this.id,
+    required this.email,
+    required this.name,
+    this.picture,
+  });
+
+  Map<String, Object?> toMap() {
+    return {
+      'id': id,
+      'email': email,
+      'name': name,
+      'picture': picture,
+    };
+  }
+
+  static CurrentUserEntity fromMap(Map<String, dynamic> map) {
+    return CurrentUserEntity(
+      id: map['id'] as String,
+      email: map['email'] as String,
+      name: map['name'] as String,
+      picture: map['picture'] as String?,
+    );
+  }
+
+  @override
+  List<Object?> get props => [id, email, name, picture];
+
+  @override
+  String toString() {
+    return '''UserEntity: {
+      'id': $id,
+      'email': $email,
+      'name': $name,
+      'picture': $picture,
+}''';
+  }
+}
--- packages/user_repository/lib/src/models/current_user.dart
+++ packages/user_repository/lib/src/models/current_user.dart
@@ -1,1 +1,65 @@
-class CurrentUser {}
+import 'package:equatable/equatable.dart';
+import 'package:user_repository/user_repository.dart';
+
+class CurrentUser extends Equatable {
+  final String id;
+  final String email;
+  final String name;
+  final String? picture;
+
+  const CurrentUser({
+    required this.id,
+    required this.email,
+    required this.name,
+    this.picture,
+  });
+
+  static const empty = CurrentUser(
+    id: '',
+    email: '',
+    name: '',
+    picture: '',
+  );
+
+  /// to modify CurrentUser
+  CurrentUser copyWith({
+    String? id,
+    String? email,
+    String? name,
+    String? picture,
+  }) {
+    return CurrentUser(
+      id: id ?? this.id,
+      email: email ?? this.email,
+      name: name ?? this.name,
+      picture: picture ?? this.picture,
+    );
+  }
+
+  /// convenience getter to check if current user is empty
+  bool get isEmpty => this == CurrentUser.empty;
+
+  /// convenience getter to check if current user is not empty
+  bool get isNotEmpty => this != CurrentUser.empty;
+
+  CurrentUserEntity toEntity() {
+    return CurrentUserEntity(
+      id: id,
+      email: email,
+      name: name,
+      picture: picture ?? picture,
+    );
+  }
+
+  CurrentUser fromEntity(CurrentUserEntity entity) {
+    return CurrentUser(
+      id: entity.id,
+      email: entity.email,
+      name: entity.name,
+      picture: entity.picture,
+    );
+  }
+
+  @override
+  List<Object?> get props => [id, email, name, picture];
+}
