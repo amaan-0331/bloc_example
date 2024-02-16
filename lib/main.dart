190a781f70e82d83725fae6e6dc43c834f43a5c8
amaan-0331 <127935561+amaan-honorfx@users.noreply.github.com>
2024-02-16 18:15:21 +0400

    feat: connect AuthenticationBloc to MainApp

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:user_repository/user_repository.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,5 +12,5 @@ void main() async {
   Bloc.observer = BlocObserverForLogging();
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
-  runApp(const MainApp());
+  runApp(MainApp(FirebaseUserRepository()));
 }
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,17 +1,36 @@
+import 'package:bloc_example/src/blocs/authentication_bloc/authentication_bloc.dart';
 import 'package:bloc_example/src/theme.dart';
 import 'package:bloc_example/src/views/home_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:user_repository/user_repository.dart';
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp(this.userRepository, {super.key});
+  final UserRepository userRepository;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      darkTheme: AppTheme.darkTheme(),
-      theme: AppTheme.lightTheme(),
-      themeMode: ThemeMode.light,
-      home: const HomeView(),
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider(
+          create: (_) => AuthenticationBloc(userRepository: userRepository),
+        ),
+      ],
+      child: MaterialApp(
+        darkTheme: AppTheme.darkTheme(),
+        theme: AppTheme.lightTheme(),
+        themeMode: ThemeMode.light,
+        home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+          builder: (context, state) {
+            if (state.status == AuthenticationStatus.authenticated) {
+              return const HomeView();
+            } else {
+              return const HomeView();
+            }
+          },
+        ),
+      ),
     );
   }
 }
